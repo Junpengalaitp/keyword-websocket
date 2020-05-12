8f1b2ad48989e83d4e5d600952ffa4244a75dd26
Junpeng He <hejunpeng2012@hotmail.com>
2020-05-12 21:11:05 +0800

    init commit

---
+++ src/test/java/com/alaitp/keyword/websocket/KeywordWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaitp.keyword.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
