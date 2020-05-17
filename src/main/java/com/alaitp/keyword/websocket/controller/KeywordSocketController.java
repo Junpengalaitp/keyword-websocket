cdcfb298e19bfe7c871e1f0aa8a1e9a63380bfbb
Junpeng He <hejunpeng2012@hotmail.com>
2020-05-17 21:24:03 +0800

    web socket configured

---
+++ src/main/java/com/alaitp/keyword/websocket/controller/KeywordSocketController.java
@@ -0,0 +1,9 @@
+package com.alaitp.keyword.websocket.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/keyword-socket")
+public class KeywordSocketController {
+}
