1e31615e3f5175455338c79d88e59cd5401a9971
Junpeng He <hejunpeng2012@hotmail.com>
2020-05-25 21:03:50 +0800

    config stomp websocket server

--- src/main/java/com/alaitp/keyword/websocket/config/WebSocketConfig.java
+++ src/main/java/com/alaitp/keyword/websocket/config/WebSocketConfig.java
@@ -1,17 +1,25 @@
 package com.alaitp.keyword.websocket.config;
 
-import com.alaitp.keyword.websocket.web.socket.JobKeywordSocket;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(new JobKeywordSocket(), "/keyword").setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/queue");
     }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/endpoint").setAllowedOrigins("*").withSockJS();
+    }
+
 }
---
+++ src/main/java/com/alaitp/keyword/websocket/controller/WsController.java
@@ -0,0 +1,44 @@
+package com.alaitp.keyword.websocket.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Slf4j
+@Controller
+public class WsController {
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+    private static final DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss sss");
+    //映射客户端"/hello"请求
+    @MessageMapping(value = "/hello")
+    //向订阅了"/topic/hello"主题的客户端广播消息
+    @SendTo(value = "/topic/hello")
+    public String responseMsg(String msg) {  //msg->客户端传来的消息
+        return msg + "world";
+    }
+
+    @Scheduled(fixedRate = 1000*10) //设置定时器，每隔10s主动向客户端(订阅了"/topic/hello"主题)发送消息
+    @SendTo("/topic/hello")
+    public void scheduleSendMsg() {
+        Date now = new Date();
+        messagingTemplate.convertAndSend("/topic/hello", df.format(now).toString());//@注解4
+        log.info(now.toString());
+    }
+    //点对点通信
+    @Scheduled(fixedRate = 1000*10)
+    public void scheduleSendMsgToUser() {
+        Date now = new Date();
+        int userId = 1;
+        messagingTemplate.convertAndSendToUser(userId+"","/queue/hello", df.format(now));
+        log.info(now.toString());
+    }
+}
