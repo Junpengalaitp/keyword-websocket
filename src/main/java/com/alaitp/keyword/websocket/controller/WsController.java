d763d2de4d002b304ead63820ab63fcb1f1934a9
Junpeng He <hejunpeng2012@hotmail.com>
2020-05-26 07:30:16 +0800

    websocket to frontend working

--- src/main/java/com/alaitp/keyword/websocket/controller/WsController.java
+++ src/main/java/com/alaitp/keyword/websocket/controller/WsController.java
@@ -5,12 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Slf4j
 @Controller
@@ -26,19 +24,19 @@ public class WsController {
         return msg + "world";
     }
 
-    @Scheduled(fixedRate = 1000*10) //设置定时器，每隔10s主动向客户端(订阅了"/topic/hello"主题)发送消息
-    @SendTo("/topic/hello")
-    public void scheduleSendMsg() {
-        Date now = new Date();
-        messagingTemplate.convertAndSend("/topic/hello", df.format(now).toString());//@注解4
-        log.info(now.toString());
-    }
-    //点对点通信
-    @Scheduled(fixedRate = 1000*10)
-    public void scheduleSendMsgToUser() {
-        Date now = new Date();
-        int userId = 1;
-        messagingTemplate.convertAndSendToUser(userId+"","/queue/hello", df.format(now));
-        log.info(now.toString());
-    }
+//    @Scheduled(fixedRate = 1000*10) //设置定时器，每隔10s主动向客户端(订阅了"/topic/hello"主题)发送消息
+//    @SendTo("/topic/hello")
+//    public void scheduleSendMsg() {
+//        Date now = new Date();
+//        messagingTemplate.convertAndSend("/topic/hello", df.format(now));
+//        log.info(now.toString());
+//    }
+//    //点对点通信
+//    @Scheduled(fixedRate = 1000*10)
+//    public void scheduleSendMsgToUser() {
+//        Date now = new Date();
+//        int userId = 1;
+//        messagingTemplate.convertAndSendToUser(userId+"","/queue/hello", df.format(now));
+//        log.info(now.toString());
+//    }
 }
--- src/main/java/com/alaitp/keyword/websocket/message/MsgReceiver.java
+++ src/main/java/com/alaitp/keyword/websocket/message/MsgReceiver.java
@@ -4,14 +4,20 @@ import com.alaitp.keyword.websocket.dto.JobKeywordDto;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class MsgReceiver {
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     @RabbitListener(queues = "${keyword.queue}")
     public void onMessage(String msg) {
         JobKeywordDto jobKeywordDto = JSON.parseObject(msg, JobKeywordDto.class);
+        messagingTemplate.convertAndSend("/topic/hello", JSON.toJSONString(jobKeywordDto));
         log.info("received message: {}", jobKeywordDto);
     }
 }
