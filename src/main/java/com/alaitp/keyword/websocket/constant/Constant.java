9b0dc136dc82fb2555b32ddc58b0aaa716aeec48
Junpeng He <hejunpeng2012@hotmail.com>
2020-06-21 21:38:27 +0800

    more categories

--- src/main/java/com/alaitp/keyword/websocket/constant/Constant.java
+++ src/main/java/com/alaitp/keyword/websocket/constant/Constant.java
@@ -2,17 +2,25 @@ package com.alaitp.keyword.websocket.constant;
 
 import java.util.Map;
 
+import static java.util.Map.entry;
+
 public class Constant {
-    public static final Map<String, String> combinedCategoryMap = Map.of(
-            "PROGRAMMING_LANGUAGE", "languages",
-            "OTHER_LANGUAGE", "languages",
-            "LIBRARY", "libraryFramework",
-            "FRAMEWORK", "libraryFramework",
-            "DATA_STORAGE", "data",
-            "DATA_TRANSMISSION", "data",
-            "DIVISION", "division",
-            "PLATFORM", "platform",
-            "APPROACH", "approach",
-            "SOFTWARE_ENGINEERING", "swe"
+    public static final Map<String, String> combinedCategoryMap = Map.ofEntries(
+            entry("PROGRAMMING_LANGUAGE", "languages"),
+            entry("OTHER_LANGUAGE", "languages"),
+            entry("LIBRARY", "libraryFramework"),
+            entry("FRAMEWORK", "libraryFramework"),
+            entry("DATA_STORAGE", "data"),
+            entry("DATA_TRANSMISSION", "data"),
+            entry("DIVISION", "division"),
+            entry("POSITION", "division"),
+            entry("PLATFORM", "platform"),
+            entry("APPROACH", "approach"),
+            entry("SOFTWARE_ENGINEERING", "swe"),
+            entry("GENERAL", "general"),
+            entry("SOFT_SKILL", "general"),
+            entry("PROTOCOL", "protocol"),
+            entry("COMPUTER_SCIENCE", "computerScience"),
+            entry("AI", "computerScience")
     );
 }
