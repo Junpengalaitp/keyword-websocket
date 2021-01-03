c766ea03e45b772f2453ec1e276277a159056abc
Junpeng He <hejunpeng2012@hotmail.com>
2021-01-03 15:51:39 +0800

    config(k8s): use k8s config map for keyword-websocket

--- src/main/java/com/alaitp/keyword/websocket/KeywordWebsocketApplication.java
+++ src/main/java/com/alaitp/keyword/websocket/KeywordWebsocketApplication.java
@@ -3,12 +3,12 @@ package com.alaitp.keyword.websocket;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @EnableScheduling
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class KeywordWebsocketApplication {
     public static void main(String[] args) {
