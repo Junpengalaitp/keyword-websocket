4429248e32c33c74000efe5cbafad336ad675a1b
Junpeng.He <hejunpeng2012@hotmail.com>
2021-01-29 16:53:47 +0800

    feat(log): upgrade init logging

--- src/main/java/com/alaitp/keyword/websocket/KeywordWebsocketApplication.java
+++ src/main/java/com/alaitp/keyword/websocket/KeywordWebsocketApplication.java
@@ -3,8 +3,12 @@ package com.alaitp.keyword.websocket;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationContextInitializedEvent;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.lang.management.ManagementFactory;
 
 @Slf4j
 @EnableScheduling
@@ -12,18 +16,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class KeywordWebsocketApplication {
     public static void main(String[] args) {
-        initLogging();
         SpringApplication.run(KeywordWebsocketApplication.class, args);
     }
 
-    private static void initLogging() {
-        String javaVersion = System.getProperty("java.version", "not specified");
-        String osName = System.getProperty("os.name", "not specified");
-        String userHome = System.getProperty("user.home", "not specified");
-        String javaHome = System.getProperty("java.home", "not specified");
-        log.info("** Your OS name is : " + osName);
-        log.info("** The version of the JVM you are running is : " + javaVersion);
-        log.info("** Your user home directory is : " + userHome);
-        log.info("** Your JRE installation directory is : " + javaHome);
+    @EventListener(ApplicationContextInitializedEvent.class)
+    public void initLogging() {
+        String defaultVal = "not specified";
+        log.info("** Your OS name is : " + System.getProperty("os.name", defaultVal));
+        log.info("** The version of the JVM you are running is : " + System.getProperty("java.version", defaultVal));
+        log.info("** Your user home directory is : " + System.getProperty("user.home", defaultVal));
+        log.info("** Your JRE installation directory is : " + System.getProperty("java.home", defaultVal));
+        log.info("** Amount of Your CPU cores : " + Runtime.getRuntime().availableProcessors());
+        log.info("** Amount of Your JVM memory: " + (double) ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getInit() / 1073741.824 + "MB");
     }
 }
